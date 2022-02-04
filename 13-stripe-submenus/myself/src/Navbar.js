f08496d95d8ee2fd7f5b6679d8a5b32a7c9dbb74
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-02-04 10:40:35 +0400

    lesson-13

---
+++ 13-stripe-submenus/myself/src/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from './images/logo.svg'
+import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  const displaySubmenu = (e) => {
+    openSubmenu();
+  }
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} classname='nav-logo' alt="stripe" />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>products</button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>developers</button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>company</button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>Sign in</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
